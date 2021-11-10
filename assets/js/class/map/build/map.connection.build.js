daa71ef9b5322eaf961f0d44327b8947e086a3e1
bemuse01 <sequeland@gmail.com>
2021-11-10 16:32:46 +0900

    update

--- assets/js/class/map/build/map.connection.build.js
+++ assets/js/class/map/build/map.connection.build.js
@@ -7,9 +7,11 @@ export default class{
     constructor({group}){
         this.param = {
             color: 0x32eaff,
-            seg: 100
+            seg: 360,
+            count: 2
         }
 
+        this.deg = 180 / this.param.seg
         this.draw = 0
 
         this.init(group)
@@ -27,52 +29,35 @@ export default class{
         const positionGroup = new THREE.Group()
         this.wrapper = new THREE.Group()
 
-        const r1 = ~~(Math.random() * COORDS.coordinates.length)
-        const r2 = ~~(Math.random() * COORDS.coordinates.length)
-        const p1 = COORDS.coordinates[r1]
-        const p2 = COORDS.coordinates[r2]
-        const pp1 = {x: p1.rx * CHILD_PARAM.width, y: p1.ry * -CHILD_PARAM.height}
-        const pp2 = {x: p2.rx * CHILD_PARAM.width, y: p2.ry * -CHILD_PARAM.height}
+        for(let i = 0; i < this.param.count; i++){
+            const {cx, cy, theta, radius} = METHOD.getCircleProp({coords: COORDS, ...CHILD_PARAM})
+            // console.log(theta)
 
-        const {rx, ry} = METHOD.getCenterPoint(p1, p2)
-        const cx = rx * CHILD_PARAM.width
-        const cy = ry * -CHILD_PARAM.height
-        const dist = Math.sqrt((pp1.x - pp2.x) ** 2 + (pp1.y - pp2.y) ** 2) / 2
-        const deg = 180 / this.param.seg
+            const mesh = this.createLineMesh(radius)
+            mesh.position.set(cx, cy, 0)
+            mesh.rotation.x = 90 * RADIAN
+            mesh.rotation.y = theta
 
-        const pp3 = pp1.y > pp2.y ? pp1 : pp2
-        const v1 = new THREE.Vector2(pp3.x - cx, pp3.y - cy)
-        const v2 = new THREE.Vector2(pp3.x + cx, 0)
-        const out = v1.x * v2.x + v1.y * v2.y
-        const dot = Math.sqrt(v1.x ** 2 + v1.y ** 2) * Math.sqrt(v2.x ** 2 + v2.y ** 2)
-        const theta = Math.acos(out / dot)
-
-        // console.log(theta)
-
-        const mesh = this.createLineMesh({dist, deg})
-        mesh.position.set(cx, cy, 0)
-        mesh.rotation.x = 90 * RADIAN
-        mesh.rotation.y = theta
-        // mesh.position.y = CHILD_PARAM.y
+            positionGroup.add(mesh)
+        }
 
         positionGroup.position.set(CHILD_PARAM.width / -2, CHILD_PARAM.height / 2 + CHILD_PARAM.y, 50)
 
-        positionGroup.add(mesh)
         this.wrapper.add(positionGroup)
         group.add(this.wrapper)
     }
     // line
-    createLineMesh({dist, deg}){
-        const geometry = this.createLineGeometry({dist, deg})
+    createLineMesh(radius){
+        const geometry = this.createLineGeometry(radius)
         const material = this.createLineMaterial()
         return new THREE.Line(geometry, material)
     }
-    createLineGeometry({dist, deg}){
+    createLineGeometry(radius){
         const position = new Float32Array(this.param.seg * 3)
 
         for(let i = 0; i < this.param.seg; i++){
-            const x = Math.cos(i * deg * RADIAN) * dist
-            const y = Math.sin(i * deg * RADIAN) * dist
+            const x = Math.cos(i * this.deg * RADIAN) * radius
+            const y = Math.sin(i * this.deg * RADIAN) * radius
             position[i * 3] = x
             position[i * 3 + 1] = y
             position[i * 3 + 2] = 0
@@ -92,15 +77,34 @@ export default class{
             transparent: true,
             depthWrite: false,
             depthTest: false
-        }) 
+        })
     }
 
 
     // tween
+    createTween(idx){
+        const start = {draw: 0}
+        const end = {draw: this.param.seg}
+
+        const tw = new TWEEN.Tween(start)
+        .to(end, 2000)
+        .onUpdate(() => this.updateTween(idx))
+        .onComplete(() => this.completeTween(idx))
+        .delay(i * 500)
+        .start()
+    }
+    updateTween(idx){
+
+    }
+    completeTween(idx){
+
+    }
 
 
     // animate
     animate(){
+        // this.draw += 2.5
 
+        // geometry.setDrawRange(0, this.draw)
     }
 }
--- assets/js/class/map/param/map.child.param.js
+++ assets/js/class/map/param/map.child.param.js
@@ -3,6 +3,5 @@ export default {
     height: 1600,
     color: 0x32eaff,
     size: 6,
-    rotation: -65,
     y: 100
 }
