2a6e40c568a01ac574d52fdd329108eac1024e44
bemuse01 <sequeland@gmail.com>
2021-12-04 22:11:12 +0900

    update

---
+++ assets/js/class/left/left.js
@@ -0,0 +1,67 @@
+export default class{
+    constructor(){
+        this.element = document.querySelector('.left')
+
+        this.modules = {
+        }
+
+        this.comp = {}
+
+        this.init()
+    }
+
+
+    // init
+    init(){
+        // this.initProxy()
+        this.create()
+    }
+    initProxy(){
+        const self = this
+        
+        const proxyObj = {
+        }
+
+        this.proxy = new Proxy(proxyObj, {
+            isAllTrue(obj){
+                return Object.keys(obj).every(key => obj[key] === true)
+            },
+            set(obj, prop, value){
+                obj[prop] = value
+
+                if(this.isAllTrue(obj)){
+                }
+                
+                return true
+            }
+        })
+    }
+
+
+    // create
+    create(){
+        this.createComponents()
+    }
+    createComponents(){
+        for(const module in this.modules){
+            const instance = this.modules[module]
+
+            this.comp[module] = new instance({element: this.element, proxy: this.proxy})
+        }
+    }
+
+
+    // animate
+    animate(){
+        for(const comp in this.comp){
+            if(!this.comp[comp].animate) continue
+            this.comp[comp].animate()
+        }
+    }
+
+
+    // get
+    get(name){
+        return this.comp[name].get()
+    }
+}
